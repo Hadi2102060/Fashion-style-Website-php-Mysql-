d49f6cdd416da03e33ac519b6ed3b654ba2647e0
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-05-21 16:04:10 +0600

    web-01

---
+++ footer.php
@@ -0,0 +1,44 @@
+<section class="footer">
+
+   <div class="box-container">
+      
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 01326382008</p>
+         <p> <i class="fas fa-phone"></i> 01897708391 </p>
+         <p> <i class="fas fa-envelope"></i> hadi@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Patuakhali,Barisal,Bangladesh </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <hr>
+        <p class="copyright">&copy; 2025 MyShop. All rights reserved. </p>
+
+
+</section>
